8221d5114eda837cb5615427b053ea17a0ef92ae
Alex Hare <alexandra.f.hare@gmail.com>
2019-04-29 20:15:44 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,19 +1,6 @@
 def consolidate_cart(cart)
   consol_cart = {}
   
-  cart.each do |thing|
-    thing.each do |attribute, value|
-      if consol_cart[thing] == false 
-        consol_cart[thing] ||= {}
-        consol_cart[thing][attribute] = value
-        consol_cart[thing][count] = 1 
-      elsif consol_cart[thing] == true 
-        consol_cart[thing] ||= {}
-        consol_cart[thing][attribute] = value
-        consol_cart[thing][count] +=1
-      end
-    end
-  end
   consol_cart
 end
 
